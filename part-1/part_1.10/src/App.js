56a186cc0b73b16dfb67589b636b626e3e4ef50b
Altin Hysa <altinhysa84@gmail.com>
2023-03-25 01:23:26 +0100

    1.10

---
+++ part-1/part_1.10/src/App.js
@@ -0,0 +1,66 @@
+import { useState } from 'react'
+
+const Statistics = (props) => {
+  const { good, neutral, bad, allClicks, average, positive } = props;
+  if(allClicks===0){
+    return (
+      <>
+      <h1>Statistics</h1>
+      <p>No Feedback given</p>
+      </>
+    )
+  }
+
+
+  return (
+    <>
+      <h1>Statistics</h1>
+      <StatisticLine text="Good" value={good} />
+      <StatisticLine text="Neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="All" value={allClicks} />
+      <StatisticLine text="Average" value={average} />
+      <StatisticLine text="Positives" value={positive} />
+    </>
+  )
+}
+
+const StatisticLine = ({text, value}) => <p>{text} {value}</p>
+
+const Button = ({handleEvent, text}) => <button onClick={handleEvent}>{text}</button>
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [allClicks, setAll] = useState(0)
+  const average = ((-1*bad)+(good*1)) / allClicks
+  const positive = (good * 100 / allClicks) + " %"
+  const handleGood = () => {
+    setGood(good + 1);
+    setAll(allClicks + 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(neutral + 1);
+    setAll(allClicks + 1)
+  
+  }
+  const handleBad = () => {
+    setBad(bad + 1);
+    setAll(allClicks + 1)
+  }
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button handleEvent={handleGood} text="good" />
+      <Button handleEvent={handleNeutral} text="neutral" />
+      <Button handleEvent={handleBad} text="bad" />
+      <Statistics good={good} bad={bad} neutral={neutral} allClicks={allClicks} positive={positive} average={average}/>
+    </div>
+  )
+}
+
+export default App
